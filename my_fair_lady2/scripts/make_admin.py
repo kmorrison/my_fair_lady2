250378d59f601cb9ee9df67f6ed1c5671c8fbedf
Kyle Morrison <fspoon64@gmail.com>
2014-08-24 19:28:07 -0700

    Getting 500s and no asserts

--- my_fair_lady2/scripts/make_admin.py
+++ my_fair_lady2/scripts/make_admin.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
+import os
 
-from my_fair_lady.settings.production import get_env_setting
 from django.contrib.auth.models import User
 if User.objects.count() == 0:
     admin = User.objects.create(username='admin')
-    admin.set_password(get_env_setting('DJANGO_ADMIN_SU_PASSWORD'))
+    admin.set_password(os.environ['DJANGO_ADMIN_SU_PASSWORD'])
     admin.is_superuser = True
     admin.is_staff = True
     admin.save()
